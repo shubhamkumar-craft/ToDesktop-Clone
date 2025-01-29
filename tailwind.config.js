2e3cff001a59401b65ec7389303f920b0072da7a
Shubham Kumar <shubhamwork121@gmail.com>
2025-01-29 13:52:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    color:{
+      primary:"#be524b"
+    },
+    fontFamily:{
+     "shubham": ["popins","sans-serif"],
+     "shubham1":["M PLUS","popins"],
+     "body":[ "inter","san-serif"]
+ 
+ 
+    }
+    },
+  plugins: [],
+}
+
